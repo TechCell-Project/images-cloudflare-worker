afeb1c86e14e42d64166691b2a595146921f37c3
lehuygiang28 <lehuygiang28@gmail.com>
2024-01-01 22:34:41 +0700

    init(): :rocket: create worker

---
+++ index.js
@@ -0,0 +1,37 @@
+// Based on https://developers.cloudflare.com/workers/tutorials/configure-your-cdn
+
+addEventListener('fetch', (event) => {
+    event.respondWith(handleRequest(event));
+});
+
+const CLOUD_URL = `https://res.cloudinary.com/${CLOUD_NAME}/image`;
+
+async function serveAsset(event) {
+    const url = new URL(event.request.url);
+    const cache = caches.default;
+    let response = await cache.match(event.request);
+    if (!response) {
+        const cloudinaryURL = `${CLOUD_URL}${url.pathname}`;
+        response = await fetch(cloudinaryURL, { headers: event.request.headers });
+        // Cache for however long, here is 4 hours.
+        const headers = new Headers(response.headers);
+        headers.set('cache-control', `public, max-age=14400`);
+        headers.set('vary', 'Accept');
+        response = new Response(response.body, { ...response, headers });
+        event.waitUntil(cache.put(event.request, response.clone()));
+    }
+    return response;
+}
+
+async function handleRequest(event) {
+    console.log('Requesting the image');
+    if (event.request.method === 'GET') {
+        let response = await serveAsset(event);
+        if (response.status > 399) {
+            response = new Response(response.statusText, { status: response.status });
+        }
+        return response;
+    } else {
+        return new Response('Method not allowed', { status: 405 });
+    }
+}
