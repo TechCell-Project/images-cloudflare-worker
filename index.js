c1c37afda81f14c0017edfeb11f5e31d6dc5269e
lehuygiang28 <lehuygiang28@gmail.com>
2024-01-08 15:41:36 +0700

    feat: :zap: update to set origin cloudinary url to response header

--- index.js
+++ index.js
@@ -10,27 +10,35 @@ async function serveAsset(event) {
     const url = new URL(event.request.url);
     const cache = caches.default;
     let response = await cache.match(event.request);
+    const cloudinaryURL = `${CLOUD_URL}${url.pathname}`;
     if (!response) {
-        const cloudinaryURL = `${CLOUD_URL}${url.pathname}`;
         response = await fetch(cloudinaryURL, { headers: event.request.headers });
-        // Cache for however long, here is 4 hours.
-        const headers = new Headers(response.headers);
-        headers.set('cache-control', `public, max-age=14400`);
-        headers.set('vary', 'Accept');
-        response = new Response(response.body, { ...response, headers });
-        event.waitUntil(cache.put(event.request, response.clone()));
+
+        // Only cache the response if it's successful
+        if (response.status >= 200 && response.status < 300) {
+            // Cache for however long, here is 4 hours.
+            const headers = new Headers(response.headers);
+            headers.set('cache-control', `public, max-age=14400`);
+            headers.set('vary', 'Accept');
+
+            response = new Response(response.body, { ...response, headers });
+            event.waitUntil(cache.put(event.request, response.clone()));
+        }
     }
-    return response;
+    return { response, cloudinaryURL };
 }
 
 async function handleRequest(event) {
     console.log('Requesting the image');
     if (event.request.method === 'GET') {
-        let response = await serveAsset(event);
+        let { response, cloudinaryURL } = await serveAsset(event);
         if (response.status > 399) {
             response = new Response(response.statusText, { status: response.status });
         }
-        return response;
+        const clonedResponse = response.clone();
+        const newResponse = new Response(clonedResponse.body, clonedResponse);
+        newResponse.headers.append('origin-image-url', cloudinaryURL);
+        return newResponse;
     } else {
         return new Response('Method not allowed', { status: 405 });
     }
